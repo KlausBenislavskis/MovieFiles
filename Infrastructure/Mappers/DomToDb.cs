3f9c158b49489a2f5294469e7c4819007308c1e2
Klavs Benislavskis <klavs.benislavskis@inbox.lv>
2023-05-10 16:25:02 +0200

    Init of infrastrucutre + example how it is used in functions

---
+++ Infrastructure/Mappers/DomToDb.cs
@@ -0,0 +1,17 @@
+﻿using MovieFiles.Core.Models;
+
+namespace MovieFiles.Infrastructure.Mappers
+{
+    internal class DomToDb
+    {
+        internal static Scaffold.Rating Map(Rating rating)
+        {
+            return new()
+            {
+                UserId = rating.UserId,
+                MovieId = rating.MovieId,
+                Rating1 = rating.RatingValue
+            };
+        }
+    }
+}
