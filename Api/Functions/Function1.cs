b5cf094b2855b783f3e1f1490a1bc736207cb0b3
Klavs Benislavskis <klavs.benislavskis@gmail.com>
2023-05-11 11:18:13 +0200

    Update Function1.cs
    Triggering pipeline for test

--- Api/Functions/Function1.cs
+++ Api/Functions/Function1.cs
@@ -36,7 +36,7 @@ namespace MovieFiles.Api.Functions
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name = name ?? data?.name; 
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
