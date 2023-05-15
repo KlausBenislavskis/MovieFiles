adbe79f714bf9cf9665860b6936d59caa04ffcdb
lukisko <lukas.pleva@mail.com>
2023-05-15 16:49:26 +0200

    keep method alive that Andrej use and also remove ratingRepository from Movie API.

--- Core/Interfaces/ISearchMovies.cs
+++ Core/Interfaces/ISearchMovies.cs
@@ -15,5 +15,12 @@ namespace MovieFiles.Core.Interfaces
         /// <param name="page">number of page that you want to get results of</param>
         /// <returns> list of movies including information about page and number of results</returns>
         public Task<MovieList> SearchForMovies(string name,int page);
+
+        /// <summary>
+        /// this method is just for testing so that @Andrej have some data to show and get
+        /// </summary>
+        /// <param name="page">number of page to get results of</param>
+        /// <returns>List of movies that are on the particular page of the most popular movies</returns>
+        public Task<MovieList> GetTestingMovies(int page);
     }
 }
