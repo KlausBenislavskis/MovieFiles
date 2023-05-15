50f275a6217693107593507fe6104b563078845d
lukisko <lukas.pleva@mail.com>
2023-05-15 11:28:22 +0200

    make search movies functionality in functions and adapters.

---
+++ Core/Interfaces/ISearchMovies.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieFiles.Core.Models;
+
+namespace MovieFiles.Core.Interfaces
+{
+    public interface ISearchMovies
+    {
+        /// <summary>
+        /// method to acquire movies based on search name (search is infix)
+        /// </summary>
+        /// <param name="name">searched term</param>
+        /// <param name="page">number of page that you want to get results of</param>
+        /// <returns> list of movies including information about page and number of results</returns>
+        public Task<MovieList> searchForMovies(string name,int page);
+    }
+}
