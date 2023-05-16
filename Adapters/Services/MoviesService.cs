eb740c269eb9bd29d7ed6a53565228efb39704ba
Klavs Benislavskis <klavs.benislavskis@inbox.lv>
2023-05-16 20:37:45 +0200

    Refactored Nicks and Lukas approach to handling movie api

--- Adapters/Services/MoviesService.cs
+++ Adapters/Services/MoviesService.cs
@@ -1,137 +1,62 @@
-using System.Net.Http.Headers;
 using MovieFiles.Core.Interfaces;
 using MovieFiles.Core.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace MovieFiles.Adapters.Services
 {
-   
     public class MoviesService : IMoviesService
     {
-        private static String API_KEY = "MOVIE_API_KEY";
-        private String _apiKey;
-        
+        //Would move this to config if not lazy
+        private const string API_KEY = "MOVIE_API_KEY";
+        private readonly string _apiKey;
         private readonly HttpClient _httpClient;
-        private readonly String language = "en-US";
-        private readonly int page = 1;
-   
+        private const string language = "en-US";
+
         public MoviesService()
-        { 
+        {
+
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _apiKey = System.Environment.GetEnvironmentVariable(API_KEY);
+            _apiKey = Environment.GetEnvironmentVariable(API_KEY);
         }
 
-      
-        public async  Task<(IList<Movie>?, HttpResponseMessage?)> GetLatestMoviesAsync()
+        private async Task<MovieList> GetMoviesAsync(string endpoint)
         {
-            string endpoint = $"movie/latest?api_key={_apiKey}&language={language}-US&page={page}";
             HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                IList<Movie>? movieResponse = JsonConvert.DeserializeObject<IList<Movie>>(jsonResponse,
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    }
-                );
-                
-                return  (movieResponse, null);
+                return new MovieList();
             }
-            // throw new Exception($"Failed to fetch the latest movies. StatusCode: {response.StatusCode}");
-                return (null, response);
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            MovieList? movieList = MovieApiUtil.ConvertApiMessage<MovieList>(jsonResponse);
+
+            return movieList ?? new MovieList();
+
         }
 
-    
-        public async Task<(IList<Movie>?, HttpResponseMessage?)> GetNowPlayingMoviesAsync()
-        {
-            // Set the endpoint URL
-            string endpoint = $"movie/now_playing?api_key={_apiKey}&language={language}-US&page={page}";
-            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                IList<Movie>? movieResponse = JsonConvert.DeserializeObject<IList<Movie>>(jsonResponse,
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    }
-                );
-                
-                return  (movieResponse, null);
-            }
-               // throw new Exception($"Failed to fetch the latest movies. StatusCode: {response.StatusCode}");
-                return (null, response);
-            
-        }
+        public async Task<MovieList> GetNowPlayingMoviesAsync(int page)
+            => await GetMoviesAsync($"movie/now_playing?api_key={_apiKey}&language={language}-US&page={page}");
 
-    
-        public async Task<(IList<Movie>?, HttpResponseMessage?)> GetPopularMoviesAsync()
-        {
-            string endpoint = $"movie/latest?api_key={_apiKey}&language={language}&page={page}";
-            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                IList<Movie>? movieResponse = JsonConvert.DeserializeObject<IList<Movie>>(jsonResponse,
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    }
-                );
-                
-                return  (movieResponse, null);
-            }
-              //  throw new Exception($"Failed to fetch the popular movies. StatusCode: {response.StatusCode}");
-                return (null, response);
-            
-        }
+        public async Task<MovieList> GetPopularMoviesAsync(int page)
+            => await GetMoviesAsync($"movie/popular?api_key={_apiKey}&language={language}&page={page}");
 
-        public async Task<(IList<Movie>?, HttpResponseMessage?)> GetTopRatedMoviesAsync()
-        {
-            
-            string endpoint = $"movie/top_rated?api_key={_apiKey}&language={language}&page={page}";
-            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                IList<Movie>? movieResponse = JsonConvert.DeserializeObject<IList<Movie>>(jsonResponse,
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    }
-                );
-                
-                return  (movieResponse, null);
-            }
-               // throw new Exception($"Failed to fetch the latest movies. StatusCode: {response.StatusCode}");
-                return (null, response);
-            
-        }
+        public async Task<MovieList> GetTopRatedMoviesAsync(int page)
+            => await GetMoviesAsync($"movie/top_rated?api_key={_apiKey}&language={language}&page={page}");
 
-        public async Task<(IList<Movie>?, HttpResponseMessage?)> GetUpcomingMoviesAsync()
+        public async Task<MovieList> GetUpcomingMoviesAsync(int page)
+            => await GetMoviesAsync($"movie/upcoming?api_key={_apiKey}&language={language}&page={page}");
+
+        public async Task<MovieList> SearchForMovies(string name, int page)
         {
-            string endpoint = $"movie/upcoming?api_key={_apiKey}&language={language}&page={page}";
-            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
-            if (response.IsSuccessStatusCode)
+            if (string.IsNullOrEmpty(_apiKey) || page < 1)
             {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                IList<Movie>? movieResponse = JsonConvert.DeserializeObject<IList<Movie>>(jsonResponse,
-                        new JsonSerializerSettings
-                        {
-                            ContractResolver = new CamelCasePropertyNamesContractResolver()
-                        }
-                );
-                
-                return  (movieResponse, null);
+                return new MovieList();
             }
-               // throw new Exception($"Failed to fetch the upcoming movies. StatusCode: {response.StatusCode}");
-                return (null, response);
-            
+
+            return await GetMoviesAsync($"search/movie?api_key={_apiKey}&page={page}&query={name}");
         }
     }
 }
--- Core/Interfaces/IMoviesService.cs
+++ Core/Interfaces/IMoviesService.cs
@@ -1,15 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
 using MovieFiles.Core.Models;
 
 namespace MovieFiles.Core.Interfaces
 {
     public interface IMoviesService
     {
-        Task<(IList<Movie>?, HttpResponseMessage?)> GetLatestMoviesAsync();
-        Task<(IList<Movie>?, HttpResponseMessage?)> GetNowPlayingMoviesAsync();
-        Task<(IList<Movie>?, HttpResponseMessage?)> GetPopularMoviesAsync();
-        Task<(IList<Movie>?, HttpResponseMessage?)> GetTopRatedMoviesAsync();
-        Task<(IList<Movie>?, HttpResponseMessage?)> GetUpcomingMoviesAsync();
+        Task<MovieList> GetNowPlayingMoviesAsync(int page);
+        Task<MovieList> GetPopularMoviesAsync(int page);
+        Task<MovieList> GetTopRatedMoviesAsync(int page);
+        Task<MovieList> GetUpcomingMoviesAsync(int page);
+
+        /// <summary>
+        /// method to acquire movies based on search name (search is infix)
+        /// </summary>
+        /// <param name="name">searched term</param>
+        /// <param name="page">number of page that you want to get results of</param>
+        /// <returns> list of movies including information about page and number of results</returns>
+        Task<MovieList> SearchForMovies(string name, int page);
     }
 
 }
