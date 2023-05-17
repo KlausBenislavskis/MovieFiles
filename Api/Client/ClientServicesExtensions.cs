5aa62db5c6043732b3e9e7ef19697e9fbe54e9cb
Klavs Benislavskis <klavs.benislavskis@gmail.com>
2023-05-17 14:14:25 +0200

    Init of comments

--- Api/Client/ClientServicesExtensions.cs
+++ Api/Client/ClientServicesExtensions.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MovieFiles.Api.Client.Services;
 
-namespace MovieFiles.Infrastructure
+namespace MovieFiles.Api.Client
 {
     public static class ClientServicesExtensions
     {
-        public static IServiceCollection AddMovieFilesClient(this IServiceCollection services,string apiUrl, string appKey)
+        public static IServiceCollection AddMovieFilesClient(this IServiceCollection services, string apiUrl, string appKey)
         {
-            
+
             services.AddScoped<IRatingService>(provider => new RatingService(apiUrl, appKey));
             services.AddScoped<IUserService>(provider => new UserService(apiUrl, appKey));
             services.AddScoped<IMoviesService>(provider => new MoviesService(apiUrl, appKey));
-          
+
             return services;
         }
 
