036d7aab4ad463fc94b7145b37bcdf9c174db353
Klavs Benislavskis <klavs.benislavskis@inbox.lv>
2023-05-17 09:15:30 +0200

    Refactored api key injection

--- Adapters/Services/MoviesService.cs
+++ Adapters/Services/MoviesService.cs
@@ -6,20 +6,18 @@ namespace MovieFiles.Adapters.Services
 {
     public class MoviesService : IMoviesService
     {
-        //Would move this to config if not lazy
-        private const string API_KEY = "MOVIE_API_KEY";
         private readonly string _apiKey;
         private readonly HttpClient _httpClient;
         private const string language = "en-US";
 
-        public MoviesService()
+        public MoviesService(string apiKey)
         {
 
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _apiKey = Environment.GetEnvironmentVariable(API_KEY);
+            _apiKey = apiKey;
         }
 
         private async Task<MovieList> GetMoviesAsync(string endpoint)
