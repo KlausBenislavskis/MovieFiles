b7da08de1039fbc3cab4bc3a85cfae5eee09a10b
lukisko <lukas.pleva@mail.com>
2023-05-26 10:56:48 +0200

    fix modifing chaced data

--- Core/Models/GenreList.cs
+++ Core/Models/GenreList.cs
@@ -3,4 +3,13 @@ namespace MovieFiles.Core.Models;
 public class GenreList
 {
     public List<Genre> Genres { get; set; }
+    public GenreList Copy(){
+        var list = new List<Genre>();
+        foreach (Genre item in Genres){
+            list.Add(item.Copy());
+        }
+        return new GenreList(){
+            Genres = list
+        };
+    }
 }
